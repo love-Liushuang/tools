648000753f76c220d0d7b9acb14e0eb934d4a4b5
liushuangMac <502277775@qq.com>
2026-03-11 23:33:40 +0800

    feat: 顶部导航

--- client/src/components/SiteLayout.jsx
+++ client/src/components/SiteLayout.jsx
@@ -12,16 +12,9 @@ function SiteLayout() {
             <NavLink to="/" end>
               首页
             </NavLink>
-            <NavLink to="/tools/json-formatter">JSON</NavLink>
-            <NavLink to="/tools/base64">Base64</NavLink>
-            <NavLink to="/tools/text-stats">文本统计</NavLink>
             <NavLink to="/tools/text-letter">文本加密</NavLink>
-            <NavLink to="/tools/txt-diff">文本对比</NavLink>
             <NavLink to="/tools/unlock-pdf">PDF解密</NavLink>
             <NavLink to="/tools/image-convert">图片转换</NavLink>
-            <NavLink to="/tools/svg-base64">SVG Base64</NavLink>
-            <NavLink to="/tools/svg-path">SVG Path</NavLink>
-            <NavLink to="/tools/svg-preview">SVG预览</NavLink>
           </nav>
         </header>
         <Outlet />
